2e87e20cb5815377af8a9a5d699718e258ebef92
NesmelovVladimir <skypointtv@gmail.com>
2022-02-15 00:33:39 +0400

    Geom(14.02.22)

--- server/src/main/java/tz/server/controller/Controller.java
+++ server/src/main/java/tz/server/controller/Controller.java
@@ -11,6 +11,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class Controller {
